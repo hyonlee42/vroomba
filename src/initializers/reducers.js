e78d1f04a20608cd05b79d60f9d3cff3086224a4
hyonlee42 <hyon.lee@utexas.edu>
2020-10-17 20:08:23 -0500

    room/vroomba initializer

---
+++ src/initializers/reducers.js
@@ -0,0 +1,10 @@
+#import { combineReducers } from "redux";
+#import roomConfiguration from "./roomConfiguration";
+#import robotConfiguration from "./robotConfiguration";
+
+#const rootReducer = combineReducers({
+  roomConfiguration,
+  robotConfiguration
+});
+
+#export default rootReducer;
---
+++ src/initializers/robot-configuration.js
@@ -0,0 +1,61 @@
+#import * as types from "../actions/ActionTypes";
+
+#const initialState = {
+  removedDirtCount: 0,
+  dirtLocations: [],
+  robotPosition: [0, 0],
+  directions: [],
+  timeoutID: 0,
+  hasCompletedAnimation: false
+};
+
+#const robotConfiguration = (state = initialState, action) => {
+  switch (action.type) {
+    case types.SET_ROBOT_COORDINATES:
+      return { ...state, robotPosition: action.robotPosition };
+    case types.ROBOT_NAV_STEP_COMPLETED:
+      return {
+        ...state,
+        directions: action.directions.slice(1),
+        timeoutID: action.timeoutID
+      };
+    case types.ROBOT_ANIM_STARTED:
+      return {
+        ...state,
+        timeoutID: action.timeoutID,
+        hasCompletedAnimation: false
+      };
+    case types.ROBOT_ANIM_ENDED:
+      return {
+        ...state,
+        hasCompletedAnimation: true
+      };
+    case types.ROBOT_SPEC_UPDATED:
+      return {
+        ...state,
+        robotPosition: action.newRoomState.robotPosition,
+        removedDirtCount: 0,
+        dirtLocations: action.newRoomState.dirtLocations,
+        directions: action.newRoomState.directions
+      };
+    case types.REMOVE_DIRT_PATCH:
+      const robotLoc = action.robotPosition;
+      const dirtLocations = state.dirtLocations;
+      const newDirtLocations = dirtLocations.filter(
+        patch => !(patch[0] === robotLoc[0] && patch[1] === robotLoc[1])
+      );
+
+      if (newDirtLocations.length !== dirtLocations.length)
+        state.removedDirtCount++;
+
+      return {
+        ...state,
+        dirtLocations: newDirtLocations,
+        removedDirtCount: state.removedDirtCount
+      };
+    default:
+      return state;
+  }
+};
+
+#export default robotConfiguration;
