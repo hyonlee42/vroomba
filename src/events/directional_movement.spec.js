4ac9bcdba511edb7c0f0bee0ddac93cf27964bf2
hyonlee42 <hyon.lee@utexas.edu>
2020-10-17 20:56:10 -0500

    directional movement

---
+++ src/events/directional_movement.spec.js
@@ -0,0 +1,15 @@
+import * as types from "../../src/events/directional_movement";
+
+describe("Action Types", () => {
+  test("All action types should have different values", () => {
+    const values = Object.values(types);
+    const valueSet = new Set(values);
+    expect(values.length).toEqual(valueSet.size);
+  });
+
+  test("Action keys and values should match", () => {
+    Object.keys(types).forEach(actionType => {
+      expect(types[actionType]).toEqual(actionType);
+    });
+  });
+});
